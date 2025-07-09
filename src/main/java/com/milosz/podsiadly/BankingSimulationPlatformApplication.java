d6a476016c7f527ed0c3f24c12790392658dd8cc
Milosz Podsiadly <m.podsiadly99@gmail.com>
2025-07-09 14:12:56 +0200

    Initial commit

---
+++ src/main/java/com/milosz/podsiadly/BankingSimulationPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.milosz.podsiadly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingSimulationPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingSimulationPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/milosz/podsiadly/BankingSimulationPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milosz.podsiadly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingSimulationPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
